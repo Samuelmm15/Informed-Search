44e0a91f8d5c78e39db4c47497932b7336841026
Cheuk Kelly Ng Pante <alu0101364544@ull.edu.es>
2022-10-29 09:57:26 +0100

    Creado ficheros

---
+++ src/map.cc
@@ -0,0 +1,20 @@
+/**
+ * 
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Asignatura: Inteligencia Artificial
+ * Curso: 3º
+ * Práctica 2: Búsqueda Informada
+ * @file map.h
+ * @authors Cheuk Kelly Ng Pante (alu0101364544@ull.edu.es) 
+ *          Samuel Martín Morales (alu0101359526@ull.edu.es)
+ * @brief 
+ * @version 0.1
+ * @date 2022-10-29
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include "../include/map.h"
